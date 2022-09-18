1cfaffdaeaabe8e86012720b6295a874890c5133
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-09-18 13:58:07 -0400

    Added a fishing item class that represents anything that can be fished.

---
+++ scripts/FishingItem.gd
@@ -0,0 +1,45 @@
+# The fishing item is a kind of resource that can be loaded and referenced anywhere.
+# This means that we're allowed to create files of these resources and be able
+# to mass produce items instead of having to hard-code every one of them.
+extends Resource
+# Scripts in Godot are classes, and classes by default are attached to their node.
+# This one is special, we'll give it a name so we can refer to it elsewhere in 
+# the engine.
+class_name FishingItem
+
+# Export means that this field can be edited in the inspector.
+# We'll have a few fields for our objects, these should be pretty self explanatory.
+export(String) var name
+export(Texture) var image
+export(String) var description
+export(Array, String) var examination_quotes
+
+# Make a random number generator that allows us to pick random examination quotes.
+var rng = RandomNumberGenerator.new()
+
+
+# The init function is called when the resource is first created in the engine.
+# Let's add some default values to make sure that the game doesn't get caught 
+# off guard with missing fields and crash.
+func _init(p_name = "UNNAMED OBJECT", \
+		p_image = load("icon.png"), \
+		p_description = "NO DESCRIPTION", \
+		p_examination_quotes = []):
+	# We'll assign the initial values to the object here.
+	name = p_name
+	image = p_image
+	description = p_description
+	examination_quotes = p_examination_quotes
+	
+	print(examination_quotes.size())
+	
+	# By default, random number generators start with the same seed. To make
+	# it unique, we'll randomise the randomiser.
+	rng.randomize()
+
+
+func has_examination_quote() -> bool:
+	return examination_quotes.size() > 0
+
+func get_random_examination_quote() -> String:
+	return examination_quotes[rng.randi_range(0, examination_quotes.size() - 1)]
