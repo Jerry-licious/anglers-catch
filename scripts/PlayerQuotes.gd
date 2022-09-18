ccbd9e933221ca17ebd05cf16d3b97f5c84c82f5
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-09-18 15:13:25 -0400

    Added the PlayerQuotes resource.

---
+++ scripts/PlayerQuotes.gd
@@ -0,0 +1,7 @@
+# This is also a resource, check FishingItem to see how resources work.
+extends Resource
+class_name PlayerQuotes
+
+# This file represents things that the player says outside of examining
+# random fishing drops.
+export(String) var cannot_fish_because_player_is_moving
